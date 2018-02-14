e054654810ea1e70582b35c6d81b6cc315ba92a3
Glenn-David Daniel <d2rd@outlook.com>
2018-02-14 09:57:27 -0800

    Link setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+// import 'bootstrap/dist/css/bootstrap.css';
+import {BrowserRouter as router } from 'react-router-dom'; // new
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+function App() {
+  return (
+    <div>
+      <h1>App Component</h1>
+      <p id="paragraph">This is the App Component</p>
+      </div>
+    );
+}
+
+// changed this
+
+
+ReactDOM.render(
+<App />, 
+<router>,
+  <div>
+    <ul>
+      <li>
+        <link to="/">Home</link>
+      </li>
+      <li>
+        <link to="/about">About</link>
+      </li>
+      <li>
+        <link to="/products">Products</link>
+      </li>
+    </ul>
+    <Route path='/' cmponent={Home} exact />
+    <Route path="/about" component={About} />
+    <Route path="/products" component={Products} />
+  </div>
+</Router>,
+document.getElementById('root')
+);
+// registerServiceWorker();
+//http://localhost:3000 /
+//http://localhost:3000 /about
